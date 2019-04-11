615f871672f57c422763ff9306d4cd1a4c898aad
SarthakJariwala <jariwala@uw.edu>
2019-04-10 18:25:32 -0700

    overall analysis browser for sub browsers

---
+++ PythonGUI_apps/DataBrowser.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 10 14:41:08 2019
+
+@author: Sarthak
+"""
+
+# system imports
+from pathlib import Path
+
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui
+
+from Lifetime_analysis import Lifetime_plot_fit
+
+pg.mkQApp()
+pg.setConfigOption('background', 'w')
+
+base_path = Path(__file__).parent
+file_path = (base_path / "DataBrowser_GUI.ui").resolve()
+
+uiFile = file_path
+
+WindowTemplate, TemplateBaseClass = pg.Qt.loadUiType(uiFile)
+
+class MainWindow(TemplateBaseClass):  
+    
+    def __init__(self):
+        TemplateBaseClass.__init__(self)
+        
+        # Create the main window
+        self.ui = WindowTemplate()
+        self.ui.setupUi(self)
+        self.ui.select_comboBox.addItems(["Lifetime Analysis", "Spectrum Analysis", "UV-Vis Analysis"])
+        self.ui.load_pushButton.clicked.connect(self.load_app)
+        
+        self.show()
+
+    
+    def load_app(self):
+        
+        analysis_software = self.ui.select_comboBox.currentText()
+        
+        if analysis_software == "Lifetime Analysis":
+            self.window = Lifetime_plot_fit.MainWindow()
+            self.window.show()
+        
+        else:
+            print("not yet linked -- coming soon")
+
+def run():
+    win = MainWindow()
+    QtGui.QApplication.instance().exec_()
+    return
+
+run()
