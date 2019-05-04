c63907971a8914716838be96dfd01b30e235771b
SarthakJariwala <jariwala@uw.edu>
2019-05-03 19:12:23 -0700

    modify spec connection logic, some defensive programming to avoid for stupidity

--- PythonGUI_apps/OceanOptics_acquire/OceanOptics_acquire_plot.py
+++ PythonGUI_apps/OceanOptics_acquire/OceanOptics_acquire_plot.py
@@ -31,9 +31,10 @@ class MainWindow(TemplateBaseClass):
         self.ui = WindowTemplate()
         self.ui.setupUi(self)
         
+        self.ui.connect_checkBox.stateChanged.connect(self._handle_spectrometer_connection)
         self.ui.live_pushButton.clicked.connect(self.live)
-        self.ui.close_connection_pushButton.clicked.connect(self.close_connection)
-        self.ui.config_save_pushButton.clicked.connect(self.save_file_location)
+        
+        self.ui.path_to_folder_pushButton.clicked.connect(self.save_file_location)
         self.ui.save_single_spec_pushButton.clicked.connect(self.save_single_spec)
         
         self.ui.init_stage_pushButton.clicked.connect(self.init_piezo_stage)
@@ -42,54 +43,69 @@ class MainWindow(TemplateBaseClass):
         self.ui.abs_mov_pushButton.clicked.connect(self.abs_mov)
         self.ui.rel_mov_pushButton.clicked.connect(self.rel_mov)
         self.ui.estimate_scan_time_pushButton.clicked.connect(self.estimate_scan_time)
-        self.ui.path_to_folder_pushButton.clicked.connect(self.save_file_location)
         self.ui.start_x_y_sacan_pushButton.clicked.connect(self.x_y_scan)
         
 #        self.ui.clear_pushButton.clicked.connect(self.clear_plot)
+        self.pi_device = None
+        self.spec = None
         
-        self.spec = sb.Spectrometer(sb.list_devices()[0])
-        self.ui.status_textBrowser.setText("Ocean Optics Device Connection Initiated\n\n Device:\n\n"+str(sb.list_devices()[0]))
-        
-        self.save_filename = None
-        self.y = None
+        self.ui.status_textBrowser.setText("Welcome!\nGUI Initiated!")
         
         self.show()
     
+    def _handle_spectrometer_connection(self):
+        if self.ui.connect_checkBox.isChecked():
+            self.connect_spectrometer()
+        else:
+            self.close_connection()
+        
     def connect_spectrometer(self):
-        if self.spec is not None:
+        if self.spec is None:
             devices = sb.list_devices()
             self.spec = sb.Spectrometer(devices[0])
+            self.ui.status_textBrowser.append("Ocean Optics Device Connected!\n\n Device:\n\n"+str(sb.list_devices()[0]))
         else:
-            self.ui.status_textBrowser.setText("Already Connected")
+            self.ui.status_textBrowser.append("Already Connected")
         
     def init_piezo_stage(self):
-        self.pi_device = GCSDevice("E-710")	# Creates a Controller instant
-        self.pi_device.ConnectNIgpib(board=0,device=4) # Connect to GPIB board
-        self.ui.status_textBrowser.setText('Connected: {}'.format(self.pi_device.qIDN().strip()))
-#        print('connected: {}'.format(self.pi_device.qIDN().strip()))
-        
-        self.axes = self.pi_device.axes[0:2] # selecting x and y axis of the stage
-        
-        self.pi_device.INI()
-        self.pi_device.REF(axes=self.axes)
-        
-        self.pi_device.SVO(axes=self.axes, values=[True,True])	# Turn on servo control for both axes
-        self.ui.status_textBrowser.setText("Current Stage Position:\n{}".format(self.pi_device.qPOS(axes=self.axes)))
-#        print(self.pi_device.qPOS(axes=self.axes))
+        if self.pi_device is None:
+            self.pi_device = GCSDevice("E-710")	# Creates a Controller instant
+            self.pi_device.ConnectNIgpib(board=0,device=4) # Connect to GPIB board
+            self.ui.status_textBrowser.append('Connected: {}'.format(self.pi_device.qIDN().strip()))
+    #        print('connected: {}'.format(self.pi_device.qIDN().strip()))
+            
+            self.axes = self.pi_device.axes[0:2] # selecting x and y axis of the stage
+            
+            self.pi_device.INI()
+            self.pi_device.REF(axes=self.axes)
+            
+            self.pi_device.SVO(axes=self.axes, values=[True,True])	# Turn on servo control for both axes
+            self.ui.status_textBrowser.append("Current Stage Position:\n{}".format(self.pi_device.qPOS(axes=self.axes)))
+    #        print(self.pi_device.qPOS(axes=self.axes))
+        else:
+            self.ui.status_textBrowser.append("Piezo Stage Is Already Initialized!")
         
     def center_piezo(self):
+        if self.pi_device is None:
+            self.init_piezo_stage()
         self.pi_device.MOV(axes=self.axes, values=[50,50])
-        self.ui.status_textBrowser.setText("Piezo Stage Centered: [50x,50y]")
+        self.ui.status_textBrowser.append("Piezo Stage Centered: [50x,50y]")
     
     def current_piezo_stage_pos(self):
-        self.ui.status_textBrowser.setText("Current Stage Position:\n{}".format(self.pi_device.qPOS(axes=self.axes)))
+        if self.pi_device is None:
+            self.init_piezo_stage()
+        self.ui.status_textBrowser.append("Current Stage Position:\n{}".format(self.pi_device.qPOS(axes=self.axes)))
     
     def abs_mov(self):
+        if self.pi_device is None:
+            self.init_piezo_stage()
         x_abs_pos = self.ui.x_abs_doubleSpinBox.value()
         y_abs_pos = self.ui.y_abs_doubleSpinBox.value()
         self.pi_device.MOV(axes=self.axes, values=[x_abs_pos,y_abs_pos])
         
     def rel_mov(self):
+        if self.pi_device is None:
+            self.init_piezo_stage()
         x_rel_pos = self.ui.x_rel_doubleSpinBox.value()
         y_rel_pos = self.ui.y_rel_doubleSpinBox.value()
         self.pi_device.MVR(axes=self.axes, values=[x_rel_pos,y_rel_pos])
@@ -123,9 +139,16 @@ class MainWindow(TemplateBaseClass):
         
         total_time = total_points*(intg_time_ms*1e-3)*(scans_to_avg) # in seconds
         
-        self.ui.status_textBrowser.setText("Estimated scan time: "+str(np.float16(total_time/60))+" mins")
+        self.ui.status_textBrowser.append("Estimated scan time: "+str(np.float16(total_time/60))+" mins")
     
     def x_y_scan(self):
+        if self.pi_device is None:
+            self.init_piezo_stage()
+            
+        if self.spec is None:
+            self.ui.status_textBrowser.append("Spectrometer not connected!\nForce connecting the spectrometer...")
+            self.connect_spectrometer()
+            
         start_time = time.time()
         x_start = self.ui.x_start_doubleSpinBox.value()
         y_start = self.ui.y_start_doubleSpinBox.value()
@@ -154,14 +177,12 @@ class MainWindow(TemplateBaseClass):
         # Define empty array for saving intensities
         data_array = np.zeros(shape=(x_range*y_range,2048))
         
-        self.ui.status_textBrowser.setText("Starting Scan...")
+        self.ui.status_textBrowser.append("Starting Scan...")
         # Move to the starting position
         self.pi_device.MOV(axes=self.axes, values=[x_start,y_start])
         
-        self.ui.status_textBrowser.setText("Scan in Progress...")
+        self.ui.status_textBrowser.append("Scan in Progress...")
         
-        # This for loop works perfectly for raster x-y scan
-        # Needs a little modification for line scans! --- np.ceil fixed the error!
         k = 0 
         for i in range(y_range):
             for j in range(x_range):
@@ -183,8 +204,8 @@ class MainWindow(TemplateBaseClass):
             else:
                 self.pi_device.MVR(axes=self.axes, values=[-x_scan_size, y_step])
         
-        self.ui.status_textBrowser.setText("Scan Complete!\nSaving Data...")
-        
+        self.ui.status_textBrowser.append("Scan Complete!\nSaving Data...")
+        # TODO add ocean optics acquisition params to the save dict
         save_dict = {"Wavelengths": self.spec.wavelengths(), "Intensities": data_array,
                      "Scan Parameters":{"X scan start (um)": x_start, "Y scan start (um)": y_start,
                                         "X scan size (um)": x_scan_size, "Y scan size (um)": y_scan_size,
@@ -193,30 +214,21 @@ class MainWindow(TemplateBaseClass):
         
         pickle.dump(save_dict, open(self.save_folder+"/"+self.ui.lineEdit.text()+"_raw_PL_spectra_data.pkl", "wb"))
         
-        self.ui.status_textBrowser.setText("Data saved!\nTotal time taken:"+str(np.float16((time.time()-start_time)/60))+" mins")
-        
-        
-    
+        self.ui.status_textBrowser.append("Data saved!\nTotal time taken:"+str(np.float16((time.time()-start_time)/60))+" mins")
+          
     def save_file_location(self):
         self.save_folder = QtWidgets.QFileDialog.getExistingDirectory(self, caption="Select Folder")
-#        filename = QtWidgets.QFileDialog.getSaveFileName(self,
-#                                                         caption="NO EXTENSION IN FILENAME")
-#        self.save_filename = filename[0]
     
     def save_single_spec(self):
         save_array = np.zeros(shape=(2048,2))
-        save_array[:,0] = self.spec.wavelengths()
+        self._read_spectrometer()
         save_array[:,1] = self.y
+        save_array[:,0] = self.spec.wavelengths()
+
         np.savetxt(self.save_folder+"/"+self.ui.lineEdit.text()+".txt", save_array, fmt = '%.5f', 
                    header = 'Wavelength (nm), Intensity (counts)', delimiter = ' ')
 
     def live(self):
-            
-#        intg_time_ms = self.ui.intg_time_spinBox.value()
-#        self.spec.integration_time_micros(intg_time_ms*1e3)
-#        
-#        scans_to_avg = self.ui.scan_to_avg_spinBox.value()
-#        Int_array = np.zeros(shape=(2048,scans_to_avg))
         save_array = np.zeros(shape=(2048,2))
         save_array[:,0] = self.spec.wavelengths()
         
@@ -224,15 +236,7 @@ class MainWindow(TemplateBaseClass):
         self.ui.plot.setLabel('bottom', 'Wavelength', units='nm')
         j = 0
         while self.ui.connect_checkBox.isChecked(): # this while loop works!
-#            for i in range(scans_to_avg):
-#        
-#                data = self.spec.spectrum(correct_dark_counts=self.ui.correct_dark_counts_checkBox.isChecked())
-#                Int_array[:,i] = data[1]
-#                self.y = np.mean(Int_array, axis=-1)
-#                save_array[:,0] = self.spec.wavelengths()
-#                save_array[:,1] = self.y
-            
-            self._read_spectrometer() #substitute for above 'for' loop
+            self._read_spectrometer()
             save_array[:,1] = self.y
             
             self.ui.plot.plot(self.spec.wavelengths(), self.y, pen='r', clear=True)
@@ -259,14 +263,16 @@ class MainWindow(TemplateBaseClass):
                 self.y = np.mean(Int_array, axis=-1)
         
         else:
-            self.ui.status_textBrowser.setText("Connect to Spectrometer!")
+            self.ui.status_textBrowser.append("Connect to Spectrometer!")
+            raise Exception("Must connect to spectrometer first!")
             
             
     def close_connection(self):
-         self.spec.close()
-         self.ui.status_textBrowser.setText("Ocean Optics Device Disconnected")
-         del self.spec
-         self.spec = None
+        if self.spec is not None:
+             self.spec.close()
+             self.ui.status_textBrowser.append("Ocean Optics Device Disconnected")
+             del self.spec
+             self.spec = None
     
     def close_application(self):
         choice = QtGui.QMessageBox.question(self, 'EXIT!',
