1c6e917b5ac579987539ede1cc15f70555b69ea8
LAKESIDE\LindaT18 <lindat18@lakesideschool.org>
2019-07-16 12:05:20 -0700

    added tauc region selection

--- PythonGUI_apps/UV_Vis_analysis/uv_vis_analysis.py
+++ PythonGUI_apps/UV_Vis_analysis/uv_vis_analysis.py
@@ -30,26 +30,38 @@ class MainWindow(TemplateBaseClass):
 		self.ui = WindowTemplate()
 		self.ui.setupUi(self)
 		
-		self.ui.actionLoad_data.triggered.connect(self.open_data_file)
-		self.ui.plot_absorbance_pushButton.clicked.connect(self.plot_absorbance)
-		self.ui.plot_tauc_pushButton.clicked.connect(self.plot_tauc)
-		self.ui.export_tauc_pushButton.clicked.connect(self.export_tauc)
-
-		self.ui.correct_for_scattering_checkBox.stateChanged.connect(self.switch_correct_for_scattering)
-
-
+		#setup uv vis plot
 		self.absorbance_plot_layout = pg.GraphicsLayoutWidget()
 		self.ui.absorbance_plot_container.layout().addWidget(self.absorbance_plot_layout)
 		self.absorbance_plot = self.absorbance_plot_layout.addPlot(title="Wavelengths vs. Absorbance")
 		self.absorbance_plot.setLabel('left', 'Wavelength', unit='nm')
 		self.absorbance_plot.setLabel('bottom', 'Absorbance', unit='a.u.')
 
+		self.ui.actionLoad_data.triggered.connect(self.open_data_file)
+		self.ui.plot_absorbance_pushButton.clicked.connect(self.plot_absorbance)
+
+		#setup tauc plot
 		self.tauc_plot_layout = pg.GraphicsLayoutWidget()
 		self.ui.tauc_plot_container.layout().addWidget(self.tauc_plot_layout)
 		self.tauc_plot = self.tauc_plot_layout.addPlot(title="Tauc plot fit")
 		self.tauc_plot.setLabel('left', '($\\alpha$h$\\nu$)$^2$') #fix formatting
 		self.tauc_plot.setLabel('bottom', 'h$\\nu', unit='ev')
+		self.tauc_region = pg.LinearRegionItem()
+		self.tauc_region.setZValue(10)
+		self.tauc_plot.addItem(self.tauc_region, ignoreBounds=True)
+		self.hv_min, self.hv_max = self.tauc_region.getRegion()
+		self.ui.hv_min_spinBox.setValue(self.hv_min)
+		self.ui.hv_max_spinBox.setValue(self.hv_max)
 
+		self.ui.plot_tauc_pushButton.clicked.connect(self.plot_tauc)
+		self.ui.export_tauc_pushButton.clicked.connect(self.export_tauc)
+		self.ui.correct_for_scattering_checkBox.stateChanged.connect(self.switch_correct_for_scattering)
+
+		#tauc region widgets
+		self.ui.hv_min_spinBox.valueChanged.connect(self.update_hv_bounds_region)
+		self.ui.hv_max_spinBox.valueChanged.connect(self.update_hv_bounds_region)
+		self.tauc_region.sigRegionChanged.connect(self.update_hv_bounds_spinBox)
+		
 		self.show()
 
 	"""Open Scan Files"""
@@ -69,14 +81,23 @@ class MainWindow(TemplateBaseClass):
 		else:
 			self.Absorbance = uncorrected
 
+	def update_hv_bounds_spinBox(self):
+		self.hv_min, self.hv_max = self.tauc_region.getRegion()
+		self.ui.hv_min_spinBox.setValue(self.hv_min)
+		self.ui.hv_max_spinBox.setValue(self.hv_max)
+		
+	def update_hv_bounds_region(self):
+		self.tauc_region.setRegion((self.ui.hv_min_spinBox.value(), self.ui.hv_max_spinBox.value()))
+		
+
 	def plot_absorbance(self):
 		self.absorbance_plot.plot(self.Wavelength, self.Absorbance, pen='r', clear=True)
 
 	def plot_tauc(self):
 		hv = 1240/self.Wavelength
-		hv_min = self.ui.tauc_start_spinBox.value()
-		hv_max = self.ui.tauc_end_spinBox.value()
-		index = (hv > hv_min) & (hv < hv_max)
+		# hv_min = self.ui.tauc_start_spinBox.value()
+		# hv_max = self.ui.tauc_end_spinBox.value()
+		index = (hv > self.hv_min) & (hv < self.hv_max)
 		Alpha_hv = (self.Absorbance * hv)**2.0
 		model = np.polyfit(hv[index], Alpha_hv[index], 1) 
 		Alpha_hv_fit = hv * model[0] + model[1] #This is the linear fit
