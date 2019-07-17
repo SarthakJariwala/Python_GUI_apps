d7c5d62f1931b36262046941036daa3cdc57f61f
LAKESIDE\LindaT18 <lindat18@lakesideschool.org>
2019-07-17 15:01:39 -0700

    changed export method, region selection shows up by default

--- PythonGUI_apps/UV_Vis_analysis/uv_vis_analysis.py
+++ PythonGUI_apps/UV_Vis_analysis/uv_vis_analysis.py
@@ -4,6 +4,8 @@ import os.path
 import pyqtgraph as pg
 from pyqtgraph import exporters
 from pyqtgraph.Qt import QtCore, QtGui, QtWidgets
+import matplotlib.pyplot as plt
+
 import numpy as np
 import time
 
@@ -18,6 +20,14 @@ file_path = (base_path / "uv_vis_analysis_gui.ui").resolve()
 uiFile = file_path
 
 WindowTemplate, TemplateBaseClass = pg.Qt.loadUiType(uiFile)
+
+"""params for plotting"""
+plt.rc('xtick', labelsize = 20)
+plt.rc('xtick.major', pad = 3)
+plt.rc('ytick', labelsize = 20)
+plt.rc('lines', lw = 1.5, markersize = 7.5)
+plt.rc('legend', fontsize = 20)
+plt.rc('axes', linewidth = 3.5)
 
 class MainWindow(TemplateBaseClass):  
 	
@@ -36,11 +46,14 @@ class MainWindow(TemplateBaseClass):
 		self.absorbance_plot.setLabel('left', 'Absorbance', unit='a.u.')
 
 		self.correction_region = pg.LinearRegionItem()
-		self.correction_region.setZValue(10)
+		#self.correction_region.setZValue(10)
+		self.correction_region_min = 600
+		self.correction_region_max = 900
+		self.correction_region.setRegion((self.correction_region_min, self.correction_region_max))
 		
 		self.ui.actionLoad_data.triggered.connect(self.open_data_file)
 		self.ui.plot_absorbance_pushButton.clicked.connect(self.plot_absorbance)
-		self.ui.correct_for_scattering_checkBox.stateChanged.connect(self.switch_correction_region)
+		self.ui.clear_uvvis_pushButton.clicked.connect(self.clear_uvvis)
 		self.ui.export_uv_vis_pushButton.clicked.connect(self.export_uv_vis)
 		self.correction_region.sigRegionChanged.connect(self.update_correction_region)
 
@@ -53,6 +66,7 @@ class MainWindow(TemplateBaseClass):
 		self.tauc_plot.setLabel('left', y_label)
 
 		self.ui.plot_tauc_pushButton.clicked.connect(self.plot_tauc)
+		self.ui.clear_tauc_pushButton.clicked.connect(self.clear_tauc)
 		self.ui.export_tauc_pushButton.clicked.connect(self.export_tauc)
 
 		self.show()
@@ -63,74 +77,85 @@ class MainWindow(TemplateBaseClass):
 			self.filename = QtWidgets.QFileDialog.getOpenFileName(self)
 			self.data = np.loadtxt(self.filename[0], delimiter = ',', skiprows = 1)
 			self.Wavelength = self.data[:,0] # in nm
-			self.Absorbance = self.data[:,1] 
-			self.correction_region.setRegion((np.min(self.Wavelength), np.max(self.Wavelength)))
+			self.Absorbance = self.data[:,1]
 		except Exception as err:
 			print(format(err))
 
 	def update_correction_region(self):
 		self.correction_region_min, self.correction_region_max = self.correction_region.getRegion()
 
-	def switch_correction_region(self):
-		if self.ui.correct_for_scattering_checkBox.isChecked():
-			self.absorbance_plot.addItem(self.correction_region, ignoreBounds=True)
-			self.correction_region.show()
-		else:
-			self.correction_region.hide()
-
 	def plot_absorbance(self):
 		try:
-			Absorbance = self.Absorbance
-			if self.ui.correct_for_scattering_checkBox.isChecked() and hasattr(self, "correction_region_min"):
-				Absorbance = Absorbance - np.mean(Absorbance[(self.Wavelength>self.correction_region_min) & (self.Wavelength<self.correction_region_max)])
-			self.absorbance_plot.plot(self.Wavelength, Absorbance, pen='r', clear=True)
+			self.scatter_corrected = False
+			self.plotted_absorbance = self.Absorbance
 			if self.ui.correct_for_scattering_checkBox.isChecked():
-				self.absorbance_plot.addItem(self.correction_region, ignoreBounds=True)
-				self.correction_region.show()
+				self.scatter_corrected = True
+				self.plotted_absorbance = self.Absorbance - np.mean(self.Absorbance[(self.Wavelength>self.correction_region_min) & (self.Wavelength<self.correction_region_max)])
+			self.absorbance_plot.plot(self.Wavelength, self.plotted_absorbance, pen='r', clear=True)
+			self.absorbance_plot.addItem(self.correction_region, ignoreBounds=True)
 		except Exception as err:
 			print(format(err))
 
+	def clear_uvvis(self):
+		self.absorbance_plot.clear()
+
 	def plot_tauc(self):
 		try:
-			hv_min = self.ui.hv_min_spinBox.value()
-			hv_max = self.ui.hv_max_spinBox.value()
-			hv = 1240/self.Wavelength
-			Alpha_hv = (self.Absorbance * hv)**2.0
-			index = (hv > hv_min) & (hv < hv_max)
-			model = np.polyfit(hv[index], Alpha_hv[index], 1) 
-			Alpha_hv_fit = hv * model[0] + model[1] #This is the linear fit
-			self.tauc_plot.plot(hv, Alpha_hv, pen='r')
-			self.tauc_plot.plot(hv, Alpha_hv_fit, pen='k')
+			self.hv_min = self.ui.hv_min_spinBox.value()
+			self.hv_max = self.ui.hv_max_spinBox.value()
+			self.hv = 1240/self.Wavelength
+			self.Alpha_hv = (self.Absorbance * self.hv)**2.0
+			self.index = (self.hv > self.hv_min) & (self.hv < self.hv_max)
+			model = np.polyfit(self.hv[self.index], self.Alpha_hv[self.index], 1) 
+			self.Alpha_hv_fit = self.hv * model[0] + model[1] #This is the linear fit
+			self.tauc_plot.plot(self.hv, self.Alpha_hv, pen='r')
+			self.tauc_plot.plot(self.hv, self.Alpha_hv_fit, pen='k')
+
+			self.Eg = - model[1]/model[0]
+			self.ui.bandgap_label.setText(str(self.Eg))
+		except:
+			pass
+
+	def clear_tauc(self):
+		self.tauc_plot.clear()
+
+	def export_uv_vis(self):
+		try:
+			filename = QtWidgets.QFileDialog.getSaveFileName(self,caption="Filename with EXTENSION")
+			plt.figure(figsize=(8,6))
+			plt.tick_params(direction='out', length=8, width=3.5)
+			plt.plot(self.Wavelength, self.plotted_absorbance, linewidth = 3, color = 'r')
+			if self.scatter_corrected:
+				plt.xlim(self.correction_region_min, self.correction_region_max)
+			else:
+				plt.xlim(np.min(self.Wavelength), np.max(self.Wavelength))
+			plt.ylim(np.min(self.plotted_absorbance), np.max(self.plotted_absorbance))
+			plt.xlabel('Wavelength (nm)', fontsize = 20)
+			plt.ylabel('Absorbance (a.u.)', fontsize = 20)
+			plt.savefig(filename[0],bbox_inches='tight', dpi=300)
+			plt.close()
 		except:
 			pass
 
 	def export_tauc(self):
 		try:
-			filename_ext = os.path.basename(self.filename[0])
-			filename = os.path.splitext(filename_ext)[0] #get filename without extension
-			save_to = os.getcwd() + "\\" + filename + "_TaucPlot.tiff"
+			filename = QtWidgets.QFileDialog.getSaveFileName(self,caption="Filename with EXTENSION")
+			plt.figure(figsize=(8,6))
+			plt.tick_params(direction='out', length=8, width=3.5)
+			plt.plot(self.hv, self.Alpha_hv, linewidth = 3, color = 'r')
+			plt.plot(self.hv, self.Alpha_hv_fit, linewidth = 2, color = 'black')
+			plt.xlim(1,2)
+			plt.ylim(0, np.max(self.Alpha_hv[self.index]) + 1)
+			plt.xlabel('h$\\nu$ (eV)', fontsize = 20)
+			plt.ylabel('($\\alpha$h$\\nu$)$^2$', fontsize = 20)
+			#plt.title(Plot_title, fontsize = 20)
 
-			exporter = pg.exporters.ImageExporter(self.tauc_plot)
-			exporter.params.param('width').setValue(800, blockSignal=exporter.widthChanged)
-			exporter.params.param('height').setValue(600, blockSignal=exporter.heightChanged)
-			# save to file
-			exporter.export(save_to)
-		except Exception as err:
-			print(format(err))
-
-	def export_uv_vis(self):
-		try:
-			filename_ext = os.path.basename(self.filename[0])
-			filename = os.path.splitext(filename_ext)[0] #get filename without extension
-			save_to = os.getcwd() + "\\" + filename + "_UV_Vis_Plot.tiff"
-
-			exporter = pg.exporters.ImageExporter(self.absorbance_plot)
-			exporter.params.param('width').setValue(800, blockSignal=exporter.widthChanged)
-			exporter.params.param('height').setValue(600, blockSignal=exporter.heightChanged)
-			# save to file
-			exporter.export(save_to)
-		except Exception as err:
-			print(format(err))
+			plt.text(1.2, 1.2, r'E$_{g}$ = %.2f eV'%self.Eg, fontsize = 15)
+			plt.tight_layout()
+			plt.savefig(filename[0],bbox_inches='tight', dpi=300)
+			plt.close()
+		except:
+			pass
 
 """Run the Main Window"""
 def run():
