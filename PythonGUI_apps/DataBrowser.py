95ddb39a89b87b81641233b66d20edca0fce13cb
LAKESIDE\LindaT18 <lindat18@lakesideschool.org>
2019-08-16 15:19:03 -0700

    initial work on image analysis

--- PythonGUI_apps/DataBrowser.py
+++ PythonGUI_apps/DataBrowser.py
@@ -18,7 +18,7 @@ from FLIM_analysis import FLIM_plot
 from UV_Vis_analysis import uv_vis_analysis
 from PLQE_analysis import plqe_analysis
 from H5_Pkl import h5_pkl_view, h5_view_and_plot
-
+from Image_analysis import Image_analysis
 pg.mkQApp()
 #pg.setConfigOption('background', 'w')
 
@@ -38,7 +38,7 @@ class MainWindow(TemplateBaseClass):
         self.ui = WindowTemplate()
         self.ui.setupUi(self)
         self.ui.select_comboBox.addItems(["Lifetime Analysis", "Spectrum Analysis", "FLIM Analysis", 
-            "UV-Vis Analysis", "PLQE Analysis", "H5 View/Plot", "H5/PKL Viewer"])
+            "UV-Vis Analysis", "PLQE Analysis", "H5 View/Plot", "H5/PKL Viewer", "Image Analysis"])
         self.ui.load_pushButton.clicked.connect(self.load_app)
         
         self.show()
@@ -69,6 +69,9 @@ class MainWindow(TemplateBaseClass):
         elif analysis_software == "H5/PKL Viewer":
             app = h5_pkl_view.H5PklView(sys.argv)
             #sys.exit(app.exec_())
+        elif analysis_software == "Image Analysis":
+            self.image_window = Image_analysis.MainWindow()
+            self.image_window.show()
         
         
 
---
+++ PythonGUI_apps/Image_analysis/Image_analysis.py
@@ -0,0 +1,142 @@
+import sys
+from pathlib import Path
+import os.path
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui, QtWidgets#, QColorDialog
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+# local modules
+ 
+pg.mkQApp()
+
+base_path = Path(__file__).parent
+file_path = (base_path / "image_analysis_gui.ui").resolve()
+
+uiFile = file_path
+
+WindowTemplate, TemplateBaseClass = pg.Qt.loadUiType(uiFile)
+
+class MainWindow(TemplateBaseClass):  
+
+	def __init__(self):
+		super(TemplateBaseClass, self).__init__()
+		
+		# Create the main window
+		self.ui = WindowTemplate()
+		self.ui.setupUi(self)
+
+		#setup imageview
+		self.imv = pg.ImageView()
+		self.imv.getView().setAspectLocked(lock=False, ratio=1)
+		self.imv.getView().setMouseEnabled(x=True, y=True)
+		self.imv.getView().invertY(False)
+		self.roi = self.imv.roi
+		self.roi.translateSnap = True
+		self.roi.scaleSnap = True
+		self.calc_scaling_factor() #initialize scaling_factor
+		self.roi.snapSize = self.scaling_factor #roi snaps to multiples of scaling_factor
+		self.roi.sigRegionChanged.connect(self.line_profile_update_plot)
+		self.roi_plot = self.imv.getRoiPlot().getPlotItem()
+
+		self.ui.image_groupBox.layout().addWidget(self.imv)
+
+		#set up ui signals
+		self.ui.load_image_pushButton.clicked.connect(self.load_image)
+		self.ui.custom_pixel_size_checkBox.stateChanged.connect(self.switch_custom_pixel_size)
+
+		self.show()
+
+	def load_image(self):
+		"""
+		Prompts the user to select a text file containing image data.
+		"""
+		try:
+			file = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file', os.getcwd())
+			image = Image.open(file[0])
+			image = image.rotate(-90, expand=True)
+			image_array = np.asarray(image)
+			try:
+				width = image_array.shape[0]
+				height = image_array.shape[1]
+				x_vals = np.arange(width)
+				self.imv.setImage(img=image_array, xvals= x_vals)
+				self.calc_scaling_factor()
+				self.roi.setPos((0,0))
+				self.roi.setSize([width, height*self.scaling_factor]) #set line roi
+				self.line_profile_update_plot()
+			except:
+				pass
+		except Exception as err:
+			print(format(err))
+
+	def line_profile_update_plot(self):
+		""" Handle line profile for intensity sum viewbox """
+		#if hasattr(self, "intensity_sums"):
+		self.roi_plot.clear()
+
+		image = self.imv.getProcessedImage()
+
+		# Extract image data from ROI
+		axes = (self.imv.axes['x'], self.imv.axes['y'])
+		data, coords = self.roi.getArrayRegion(image.view(np.ndarray), self.imv.imageItem, axes, returnMappedCoords=True)
+		if data is None:
+			return
+
+		self.calc_scaling_factor()
+		x_values = coords[1][0] * self.scaling_factor #adjust x-axis roi plot
+
+		#calculate sums along columns in region
+		if len(data.shape) == 2: #if grayscale, average intensities 
+			sums_to_plot = np.mean(data, axis=0)
+			try:
+				self.roi_plot.plot(x_values, sums_to_plot)
+			except:
+				pass
+		elif len(data.shape) > 2: #if rgb arrays, plot individual components
+			r_values = data[:,:,0]
+			g_values = data[:,:,1]
+			b_values = data[:,:,2]
+			r_avg = np.mean(r_values, axis=0) #average red values across columns
+			g_avg = np.mean(g_values, axis=0) #average green values
+			b_avg = np.mean(b_values, axis=0) #average blue values
+			try:
+				self.roi_plot.plot(x_values, r_avg, pen='r')
+				self.roi_plot.plot(x_values, g_avg, pen='g')
+				self.roi_plot.plot(x_values, b_avg, pen='b')
+			except:
+				pass
+
+	def calc_scaling_factor(self):
+		"""
+		Calculate scaling factor
+		"""
+		if self.ui.custom_pixel_size_checkBox.isChecked():
+			self.scaling_factor = self.ui.custom_pixel_size_spinBox.value()
+		else:
+			pixel_size = 7.4
+			self.scaling_factor = pixel_size/int(self.ui.magnification_comboBox.currentText())
+
+	def switch_custom_pixel_size(self):
+		checked = self.ui.custom_pixel_size_checkBox.isChecked()
+		self.ui.custom_pixel_size_spinBox.setEnabled(checked)
+		self.ui.magnification_comboBox.setEnabled(not checked)
+
+	def close_application(self):
+		choice = QtGui.QMessageBox.question(self, 'EXIT!',
+											"Do you want to exit the app?",
+											QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
+		if choice == QtGui.QMessageBox.Yes:
+			sys.exit()
+		else:
+			pass
+
+"""Run the Main Window"""    
+def run():
+	win = MainWindow()
+	QtGui.QApplication.instance().exec_()
+	return win
+
+#Uncomment below if you want to run this as standalone
+#run()
